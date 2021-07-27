ac2bb9ecbeaa23478448b92e76774d34c74b7b02
Junhao Li <2535868l@student.gla.ac.uk>
2021-07-27 12:25:29 +0800

    chapter3 completed

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rango import views
+from django.urls import include
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
