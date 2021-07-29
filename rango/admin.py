c2d6e787dfde84d16954021c2a918a5d0341af5f
Junhao Li <2535868l@student.gla.ac.uk>
2021-07-29 21:36:00 +0800

    chapter6 test completed

--- rango/admin.py
+++ rango/admin.py
@@ -7,6 +7,10 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
-admin.site.register(Page, PageAdmin)
 
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+# Update the registration to include this customised interface
+admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
--- rango/urls.py
+++ rango/urls.py
@@ -4,4 +4,6 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+        views.show_category, name='show_category'),  # category_name_slug is what we pass through to the view's 'show_category()'
 ]
